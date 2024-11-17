e5679c7ecccb7e56277f97b438e80f0d41097a9b
taka <test>
2024-11-17 09:15:00 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by mac on 2024/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
